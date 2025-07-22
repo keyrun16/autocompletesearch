04d9c8ad44aaf41c85b4869766af5b41df2be86c
Vijaya Kiran <keyrun16@gmail.com>
2025-07-22 22:08:04 +0530

    Update autoComplete.js

--- autoComplete.js
+++ autoComplete.js
@@ -4,7 +4,16 @@ const Search = () => {
   const [searchdata, setData] = useState([]);
   const [inputValue, setInputValue] = useState('');
   const [inputFocus, setInputFocus] = useState(false);
+  const [cacheData, setCacheData] = useState({});
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
   const fetchData = (searchData) => {
+    if (cacheData[searchData]) {
+      console.log('from Cache', cacheData[searchData]);
+      setData(cacheData[searchData]);
+      return;
+    }
+    console.log('making API call');
     fetch(`https://dummyjson.com/recipes/search?q=${searchData}`)
       .then((result) => {
         return result.json();
@@ -12,6 +21,7 @@ const Search = () => {
       .then((jsonData) => {
         setData(jsonData.recipes);
         console.log(jsonData.recipes);
+        setCacheData((prev) => ({ ...prev, [searchData]: jsonData.recipes }));
       })
       .catch((error) => {
         console.log('Fetch data error', error);
@@ -19,8 +29,11 @@ const Search = () => {
   };
 
   useEffect(() => {
+    let timer;
+    // if (inputValue !== '') {
     //Debouncing
-    const timer = setTimeout(fetchData(inputValue), 3000);
+    timer = setTimeout(fetchData(inputValue), 3000);
+    // }
 
     return () => {
       clearTimeout(timer);
@@ -28,7 +41,7 @@ const Search = () => {
   }, [inputValue]);
 
   const onChangeHandle = (e) => {
-    console.log(e.target.value);
+    console.log('onChangeHandle', e.target.value);
     setInputValue(e.target.value);
   };
 
@@ -36,28 +49,50 @@ const Search = () => {
     setInputFocus(true);
   };
   const onBlurHandle = () => {
+    // setInputFocus(false);
+  };
+  const onClickHandle = (val) => {
+    console.log('in on click', val);
+    setInputValue(val);
     setInputFocus(false);
+  };
+  const onKeyDownHandle = (e) => {
+    console.log(e.key);
+    if (e.key === 'ArrowUp' && selectedIndex > 0) {
+      setSelectedIndex((prev) => prev - 1);
+    } else if (e.key === 'ArrowDown' && selectedIndex < searchdata.length - 1) {
+      setSelectedIndex((prev) => prev + 1);
+    } else if (e.key === 'Enter' && selectedIndex >= 0) {
+      setInputValue(searchdata[selectedIndex].name);
+    }
   };
 
   return (
     <>
-      <div>Search Data</div>
-      <input
-        className="serachInput"
-        tyepe="input"
-        onChange={(e) => onChangeHandle(e)}
-        onFocus={() => onFocusHandle()}
-        onBlur={() => onBlurHandle()}
-      />
-      {inputFocus && (
-        <div className="searchContainer">
-          {searchdata?.map((record, index) => (
-            <div key={index} className="record">
-              {record.name}
-            </div>
-          ))}
-        </div>
-      )}
+      <h1>Search Data</h1>
+      <div onBlur={() => onBlurHandle()}>
+        <input
+          className="serachInput"
+          tyepe="input"
+          onChange={(e) => onChangeHandle(e)}
+          onFocus={() => onFocusHandle()}
+          value={inputValue}
+          onKeyDown={onKeyDownHandle}
+        />
+        {inputFocus && (
+          <div className="searchContainer">
+            {searchdata?.map((record, index) => (
+              <div
+                key={index}
+                className={selectedIndex === index ? 'record active' : 'record'}
+                onClick={() => onClickHandle(record.name)}
+              >
+                {record.name}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     </>
   );
 };
